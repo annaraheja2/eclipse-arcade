b4c01b3e8d6b2e4c3eac5f9b3975cb2601e0881c
Anna Raheja <annaraheja2@gmail.com>
2026-07-22 14:51:23 -0400

    Deploy: accounts — Firebase auth + cloud-synced progress

---
+++ assets/index.esm-Cfp2uU62.js
@@ -0,0 +1,16 @@
+import{v as e}from"./index.esm-MTk5nOTw.js";import{F as o,S as t,I as n,J as _,K as v,M as A,N as m,O as g,b as S,P as b,_ as d,Q as F,u as l,z as E,R as c,T as C,h as L,U as N,V as O,W as f,X as z,Y as I}from"./index.esm-MTk5nOTw.js";var s="firebase",a="12.16.0";/**
+ * @license
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */e(s,a,"app");export{o as FirebaseError,t as SDK_VERSION,n as _DEFAULT_ENTRY_NAME,_ as _addComponent,v as _addOrOverwriteComponent,A as _apps,m as _clearComponents,g as _components,S as _getProvider,b as _isFirebaseApp,d as _isFirebaseServerApp,F as _isFirebaseServerAppSettings,l as _registerComponent,E as _removeServiceInstance,c as _serverApps,C as deleteApp,L as getApp,N as getApps,O as initializeApp,f as initializeServerApp,z as onLog,e as registerVersion,I as setLogLevel};
